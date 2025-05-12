3ca0f4095e2f27d07bec8b482485b161bd7db600
fooiskandar <fooiskandar@gmail.com>
2025-05-12 11:16:55 +0700

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,13 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  website: "https://blogie5.netlify.app/", // replace this with your deployed domain
+  author: "FoO Iskandar",
+  desc: "Weblog yang lemah dalam SEO. ",
+  title: "FoO Iskandar",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
-  postPerPage: 3,
+  postPerPage: 5,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
 };
 
@@ -17,7 +17,7 @@ export const LOCALE = {
 } as const;
 
 export const LOGO_IMAGE = {
-  enable: false,
+  enable: true,
   svg: true,
   width: 216,
   height: 46,
